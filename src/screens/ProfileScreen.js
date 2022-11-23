673bc38531e19e0f8d7e79fff79cd907413a1c4d
JeonghakHeo <jeonghak4142@gmail.com>
2022-11-24 10:38:42 +1100

    deleted unused import and clean up comments

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, Form, Button, Table } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
@@ -63,54 +62,6 @@ const ProfileScreen = () => {
       dispatch(updateUserProfile({ id: user._id, name, email, password }))
     }
   }
-  // let neededItems = []
-  // const getNeededItems = (orders) => {
-  //   let cardItems = {}
-  //   if (orders) {
-  //     console.log(orders)
-  //     orders.map((order) => {
-  //       cardItems.orderDate = order.createdAt
-  //       cardItems.orderId = order._id
-  //       cardItems.totalPrice = order.totalPrice
-  //       console.log(order.orderItems.length)
-
-  //       for (let i = 0; i < order.orderItems.length; i++) {
-  //         cardItems.productId = order.orderItems[i].product
-  //         cardItems.image = order.orderItems[i].image
-  //         cardItems.productName = order.orderItems[i].name
-  //         cardItems.productPrice = order.orderItems[i].price
-  //         cardItems.qty = order.orderItems[i].qty
-  //       }
-
-  //       // neededItems.productId = order.product
-  //       // neededItems.image = order.image
-  //       // neededItems.productName = order.name
-  //       // neededItems.productPrice = order.price
-  //       // neededItems.qty = order.qty
-
-  //       // neededItems.push(
-  //       //   {
-  //       //     orderDate: order.createdAt,
-  //       //   },
-  //       //   { orderId: order._id },
-  //       //   { totalPrice: order.totalPrice }
-  //       // )
-  //       // order.orderItems.map((order) => {
-  //       //   neededItems.push(
-  //       //     { productId: order.product },
-  //       //     { image: order.image },
-  //       //     { productName: order.name },
-  //       //     { produdctPrice: order.price },
-  //       //     { qty: order.qty }
-  //       //   )
-  //       // })
-  //     })
-  //   }
-  //   neededItems.push(cardItems)
-  //   return neededItems
-  // }
-  // getNeededItems(orders)
-  // console.log('neededItems: ', neededItems)
 
   return (
     <Row>
@@ -187,74 +138,3 @@ const ProfileScreen = () => {
 }
 
 export default ProfileScreen
-
-/* 
-  <Col md={9}>
-    <h2>My Orders</h2>
-      ** here **
-      <Table
-            striped
-            bordered
-            hover
-            responsive
-            className='table-sm text-center'
-          >
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>DATE</th>
-                <th>TOTAL</th>
-                <th>PAID</th>
-                <th>DELIVERED</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((order) => (
-                <tr key={order._id}>
-                  <td>{order._id}</td>
-                  <td>{order.createdAt.substring(0, 10)}</td>
-                  <td>${order.totalPrice}</td>
-                  <td>
-                    {order.isPaid ? (
-                      order.paidAt.substring(0, 10)
-                    ) : (
-                      <i className='fas fa-times' style={{ color: 'red' }}></i>
-                    )}
-                  </td>
-                  <td>
-                    {order.isDelivered ? (
-                      order.deliveredAt.substring(0, 10)
-                    ) : (
-                      <i className='fas fa-times' style={{ color: 'red' }}></i>
-                    )}
-                  </td>
-                  <td>
-                    <LinkContainer to={`/order/${order._id}`}>
-                      <Button variant='light' className='btn-md'>
-                        DETAILS
-                      </Button>
-                    </LinkContainer>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-    </Col>
-
-   
-*/
-
-/* {loadingOrders ? (
-          <Loader />
-        ) : errorOrders ? (
-          <Message variant='danger'>{errorOrders}</Message>
-        ) : orders.length === 0 ? (
-          <Message>Your order is empty</Message>
-        ) : (
-          orders.map((order) => (
-            <div key={Math.random()}>
-              <OrderCard order={order} />
-            </div>
-          ))
-        )} */
