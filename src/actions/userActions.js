18aef91c5c33df48868038b21d6be3d4f8050f66
JeonghakHeo <jeonghak4142@gmail.com>
2022-11-24 23:47:29 +1100

    test

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -35,6 +35,8 @@ import {
 } from '../constants/userConstants'
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
+const productionUrl = 'https://proshop-backend-ihag.onrender.com'
+
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: USER_LOGIN_REQUEST })
@@ -46,7 +48,7 @@ export const login = (email, password) => async (dispatch) => {
     }
 
     const { data } = await axios.post(
-      '/api/users/login',
+      `${productionUrl}/api/users/login`,
       { email, password },
       config
     )
@@ -88,7 +90,7 @@ export const register = (name, email, password) => async (dispatch) => {
     }
 
     const { data } = await axios.post(
-      '/api/users',
+      `${productionUrl}/api/users`,
       { name, email, password },
       config
     )
@@ -129,7 +131,7 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.get(`/api/users/${id}`, config)
+    const { data } = await axios.get(`${productionUrl}/api/users/${id}`, config)
 
     dispatch({
       type: USER_DETAILS_SUCCESS,
@@ -161,7 +163,11 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.put(`/api/users/profile`, user, config)
+    const { data } = await axios.put(
+      `${productionUrl}/api/users/profile`,
+      user,
+      config
+    )
 
     dispatch({
       type: USER_UPDATE_PROFILE_SUCCESS,
@@ -196,7 +202,7 @@ export const listUsers = () => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.get('/api/users', config)
+    const { data } = await axios.get(`${productionUrl}/api/users`, config)
 
     dispatch({
       type: USER_LIST_SUCCESS,
@@ -227,7 +233,7 @@ export const deleteUser = (id) => async (dispatch, getState) => {
       },
     }
 
-    await axios.delete(`/api/users/${id}`, config)
+    await axios.delete(`${productionUrl}/api/users/${id}`, config)
 
     dispatch({
       type: USER_DELETE_SUCCESS,
@@ -258,7 +264,11 @@ export const updateUser = (user) => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.put(`/api/users/${user._id}`, user, config)
+    const { data } = await axios.put(
+      `${productionUrl}/api/users/${user._id}`,
+      user,
+      config
+    )
 
     dispatch({ type: USER_UPDATE_SUCCESS })
 
@@ -284,7 +294,11 @@ export const sendResetToken = (email) => async (dispatch) => {
       },
     }
 
-    await axios.post(`/api/users/forgotpassword`, { email }, config)
+    await axios.post(
+      `${productionUrl}/api/users/forgotpassword`,
+      { email },
+      config
+    )
 
     dispatch({ type: USER_SEND_REST_PASSWORD_TOKEN_SUCCESS })
 
@@ -310,7 +324,7 @@ export const checkToken = (verificationCode) => async (dispatch) => {
       },
     }
     await axios.post(
-      `/api/users/forgotpassword/check`,
+      `${productionUrl}/api/users/forgotpassword/check`,
       { resetToken: verificationCode },
       config
     )
@@ -342,7 +356,7 @@ export const resetPassword = (verified, newPassword) => async (dispatch) => {
       const email = JSON.parse(localStorage.getItem('resetEmail'))
 
       await axios.put(
-        `/api/users/resetpassword`,
+        `${productionUrl}/api/users/resetpassword`,
         { email, newPassword },
         config
       )
